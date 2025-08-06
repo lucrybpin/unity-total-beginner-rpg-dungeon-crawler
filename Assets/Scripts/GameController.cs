165ca335c1463d26a0480f23b7dcd5fac068118d
lucspinto <lucspinto@outlook.com>
2025-08-06 16:42:08 -0300

    combat - refactoring player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,12 @@
-using System;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public Transform cameraTransform;
-
+    public PlayerController playerController;
     public EndOfDungeon endOfDungeon;
     public GameObject canvasEndOfGame;
+
+    private bool gameFinished = false;
 
     void Start()
     {
@@ -16,33 +16,14 @@ public class GameController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit, 4.1f))
-            {
-                cameraTransform.position += 4 * cameraTransform.forward;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (!Physics.Raycast(cameraTransform.position, -cameraTransform.forward, out RaycastHit hit, 4.1f))
-            {
-                cameraTransform.position -= 4 * cameraTransform.forward;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            cameraTransform.rotation *= Quaternion.Euler(0f, -90f, 0f);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            cameraTransform.rotation *= Quaternion.Euler(0f, 90f, 0f);
-        }
+        if(!gameFinished)
+            playerController.Move();
     }
 
     void EndGame()
     {
         canvasEndOfGame.SetActive(true);
+        gameFinished = true;
     }
 
     public void CloseGame()
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    public void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (!Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit, 4.1f))
+            {
+                cameraTransform.position += 4 * cameraTransform.forward;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (!Physics.Raycast(cameraTransform.position, -cameraTransform.forward, out RaycastHit hit, 4.1f))
+            {
+                cameraTransform.position -= 4 * cameraTransform.forward;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            cameraTransform.rotation *= Quaternion.Euler(0f, -90f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            cameraTransform.rotation *= Quaternion.Euler(0f, 90f, 0f);
+        }
+    }
+}
